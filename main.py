c3ab903ff5dd5a2979a077f3ba45d9690e380b36
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-09-27 20:31:06 -0500

    implemented paddle classes

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+paddle2 = Paddle(350)
+paddle1 = Paddle(-350)
+screen.listen()
+screen.tracer(0)
+screen.onkey(paddle1.move_paddle_up, 'w')
+screen.onkey(paddle1.move_paddle_down, 's')
+screen.onkey(paddle2.move_paddle_up, 'Up')
+screen.onkey(paddle2.move_paddle_down, 'Down')
+
+game_is_on = True
+
+while(game_is_on):
+  screen.update()
+  time.sleep(0.01)
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,7 +1,25 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-  def __init__(self):
+  def __init__(self, x):
     super().__init__()
-    self.height = 100
-    self.width = 20
+    self.shape('square')
+    self.x = x
+    self.shapesize(stretch_wid=5, stretch_len=1)
+
+    self.penup()
+    self.color('white')
+    self.goto(self.x, 0)
+    # self.listen()
+    # self.onkey(self.move_paddle_up, 'w')
+    # self.onkey(self.move_paddle_down, 's')
+
+
+  def move_paddle_up(self):
+
+    current_y = self.ycor()
+    self.setpos(self.x, current_y + 20)
+
+  def move_paddle_down(self):
+    current_y = self.ycor()
+    self.setpos(self.x, current_y - 20)
