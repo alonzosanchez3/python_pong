cd9b0b47276401d8abb27947bd6cffb2efbccbe7
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-09-28 06:19:04 -0500

    added game end detection

--- ball.py
+++ ball.py
@@ -17,8 +17,15 @@ class Ball(Turtle):
     self.setpos(x + self.x_move, y + self.y_move)
 
 
-  def bounce(self):
+  def bounce_y(self):
     self.y_move *= -1
+
+  def bounce_x(self):
+    self.x_move *= -1
+
+  def reset_position(self):
+    self.goto(0,0)
+    self.bounce_x()
 
 
 
--- main.py
+++ main.py
@@ -24,7 +24,17 @@ while(game_is_on):
   screen.update()
   ball.move()
   if ball.ycor() >= 280 or ball.ycor() <= -280:
-    ball.bounce()
+    ball.bounce_y()
+
+  if ball.distance(paddle2) < 50 and ball.xcor() > 320 or ball.distance(paddle1) < 50 and ball.xcor() < -320:
+    ball.bounce_x()
+
+  if ball.xcor() > 380:
+    ball.reset_position()
+
+  if ball.xcor() < -380:
+    ball.reset_position()
+
 
 
 
